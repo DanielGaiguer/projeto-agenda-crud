98569374ced1857791a0dde6754b74c83227e189
DanielGaiguer <gaiguer2019@gmail.com>
2025-02-11 20:05:17 -0300

    Update server.js

--- server.js
+++ server.js
@@ -11,7 +11,7 @@ mongoose.connect(process.env.CONNECTIONSTRING, {
     app.emit('pronto');
   })
   .catch(e => console.log(e));
-const session = require('express-session');
+const session = require('express-session'); 
 const MongoStore = require('connect-mongo').default; 
 const flash = require('connect-flash');
 const routes = require('./routes');
