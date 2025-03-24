d0ced26de28d4d2f195ee501ec22cf3c5d731e04
Daniel Gaiguer <gaiguer2019@gmail.com>
2025-03-24 18:28:49 -0300

    Update homeController.js

--- src/controllers/homeController.js
+++ src/controllers/homeController.js
@@ -4,7 +4,6 @@ const Contato = require('../models/ContatoModel')
 exports.index = async (req, res) => {
     if (req.session.user) {
         email = req.session.user.email
-        console.log(email)
 
         const contatos = await Contato.buscaContatos({ usuario: email }) 
         res.render('index', { contatos });
